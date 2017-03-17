29267508cb3576955ed5cc1f08fdd5e31689687e
BrunoOliv <brunooliveira.xp@gmail.com>
2017-03-17 20:54:46 -0300

    ler e imprime vetor 10

---
+++ atividade1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void InfImp () {
+    int vetor[10];
+  
+  printf("Informe 10 valores p/ armanezar (separados por espaco): ");
+    scanf("%d %d %d %d %d %d %d %d %d %d", &vetor[0], &vetor[1], &vetor[2], &vetor[3], &vetor[4], &vetor[5], &vetor[6], &vetor[7], &vetor[8], &vetor[9]);
+    
+    printf("\n\nResultado:\n");
+     for (int i=0; i<10; i++) {
+      printf("Vetor [%d]: %d\n", i+1, vetor[i]);
+    }
+}
+
+int main () {
+
+    InfImp ();
+    
+  return 0;
+}
