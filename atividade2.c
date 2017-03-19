04e598499c597cfb8229e3a7a2e64884f607bcc5
BrunoOliv <brunooliveira.xp@gmail.com>
2017-03-18 21:52:30 -0300

    ler/imprimi - media

---
+++ atividade2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int lerTam () {
+    int tamV;
+
+    do {
+        printf("Informe o tamanho do vetor (<= 20): ");
+        scanf("%d", &tamV);
+
+        if (tamV > 20) {
+        printf("Erro! Tamanho acima do permitido.");
+        }
+    }while (tamV <= 0 && tamV > 20);
+    return tamV;
+}
+
+int *alocVet () {
+
+    int *ptrVetor = (int*) malloc (20 * sizeof(int));
+
+    return  ptrVetor;
+}
+
+void lerVals (int *ptrVetor, int tamV) {
+    for (int i=0; i<tamV; i++) {
+        printf("Informe o valor [%d]: ", i+1);
+        scanf("%d", &ptrVetor[i]);
+    }
+}
+
+void medImp(int *ptrVetor, int tamV) {
+    int soma=0;
+    float med;
+
+    for(int i=0; i<tamV; i++) {
+    soma = soma + ptrVetor[i];
+    }
+    med = soma / tamV;
+
+    printf("Media: %.2f", med);
+}
+
+int main () {
+
+    int tamV = lerTam();
+
+    int *ptrVetor = alocVet();
+
+    lerVals (ptrVetor, tamV);
+
+    medImp (ptrVetor, tamV);
+
+    free(ptrVetor);
+    return 0;
+}
