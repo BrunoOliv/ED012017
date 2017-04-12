ab8ebf503b88f7fa1fef48d7e1fd46555b6ef4bd
BrunoOliv <brunooliveira.xp@gmail.com>
2017-04-12 20:25:03 -0300

    exercicio - pilha

---
+++ pilha.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int verif (hPilha *pilha) {
+    if (pilha.topo == 0) {
+        return true;
+    }
+}
+
+void push (hPilha *pilha, int valor) {
+    pilha.topo = pilha.topo + 1;
+    pilha.vetor[pilha.topo] = valor;
+}
+
+void pop (hpilha *pilha) {
+    if(verif(&pilha) == true) {
+        printf
+    }
+}
+
+struct typedef hPilha {
+    int vetor [10];
+    int topo=0;
+};
+
+int main {
+    int valor;
+    hpilha pilha;
+
+
+
+
+
+
+}
