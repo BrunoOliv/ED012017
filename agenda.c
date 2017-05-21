384ceed40ac87935bd35c0eb0fda5954f7c28e0a
BrunoOliv <brunooliveira.xp@gmail.com>
2017-05-21 00:56:51 -0300

    Trabalho 2 - Agenda

--- agenda.c
+++ agenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct lista Lista;
 typedef struct listano ListaNo;
@@ -9,8 +10,8 @@ struct lista {
 };
 
 struct listano {
-    char nome[60];
-    char email[60];
+    char nome[40];
+    char email[40];
     char telefone[20];
     ListaNo* prox;
 };
@@ -21,29 +22,147 @@ Lista* criaLista () {
     return l;
 }
 
+//opção 0 do menu()
+void liberaAgenda(Lista *l){
+
+    if(l->prim == NULL)
+        printf("\nA agenda esta vazia!\n");
+
+    else {
+        ListaNo* p = l->prim;
+        while(p != NULL) {
+            ListaNo* t = p->prox;
+            free(p);
+            p = t;
+        }
+    free(l);
+    }
+
+}
+
+//opção 1 do menu()
 void insereContato (Lista* l) {
     ListaNo* novo = (ListaNo*) malloc (sizeof(ListaNo));
 
     printf("\nInforme o nome do novo contato: ");
-    scanf("%s", novo->nome);
-
+    getchar();
+    scanf("%[^\n]", novo->nome);
+    getchar();
 
     printf("\nInforme o email do novo contato: ");
-    scanf("%s", novo->email);
-
+    scanf("%[^\n]", novo->email);
+    getchar();
 
     printf("\nInforme o telefone do novo contato: ");
-    scanf("%s", novo->telefone);
-
+    scanf("%[^\n]", novo->telefone);
+    getchar();
 
     novo->prox = l->prim;
     l->prim = novo;
 }
 
+//opção 2 do menu()
 void imprimeContatos (Lista* l) {
-    printf("Nome(s)                    Email(s)                   Telefone(s)");
+    printf("\n\nNOME(s)                         EMAIL(s)                        TELEFONE(s)\n\n");
     for (ListaNo* p=l->prim; p!=NULL; p=p->prox)
-    printf("\n%s                  %s                  %s", p->nome, p->email, p->telefone);
+    printf("%s                  %s                  %s\n", p->nome, p->email, p->telefone);
+}
+
+//opção 3 do menu()
+void busImp (Lista* l) {
+    char nome[40];
+    int cont=1, ret;
+    printf("\nInforme o nome: ");
+    getchar();
+    scanf("%[^\n]", &nome);
+    getchar();
+
+    ListaNo* p = l->prim;
+    do {
+        ret = strncmp(p->nome, nome, 7);
+        if(ret == 0){
+            printf("\n\nNOME(s)                         EMAIL(s)                        TELEFONE(s)\n\n");
+            printf("%s                  %s                  %s\n", p->nome, p->email, p->telefone);
+            cont=0;
+        }
+        else
+        p=p->prox;
+    }while(cont != 0 && p != NULL);
+    if(cont == 1)
+        printf("\n\nNome nao encontrado!\n\n");
+}
+
+//opção 4 do menu()
+void editRemov (Lista* l){
+    int opc;
+    printf("Informe 1 p/ editar ou 2 p/ excluir um contato: ");
+    scanf("%d", &opc);
+
+    ListaNo* p = l->prim;
+
+    if (opc == 1) {
+    char nome[40];
+    int cont=1, ret;
+        printf("\nInforme o nome do contato para editar: ");
+        getchar();
+        scanf("%[^\n]", &nome);
+        getchar();
+
+    do {
+        ret = strncmp(p->nome, nome, 7);
+        if(ret == 0){
+        printf("\nInforme o nome novo: ");
+        getchar();
+        scanf("%[^\n]", p->nome);
+        getchar();
+
+        printf("\nInforme o email novo: ");
+        scanf("%[^\n]", p->email);
+        getchar();
+
+        printf("\nInforme o telefone novo: ");
+        scanf("%[^\n]", p->telefone);
+        getchar();
+
+        cont=0;
+        }
+        else
+        p=p->prox;
+    }while(cont != 0 && p != NULL);
+        if(cont == 1)
+        printf("\n\nNome nao encontrado!\n\n");
+
+    }
+
+    else if (opc == 2) {
+        char nome[40];
+        int cont=1, ret;
+        ListaNo* ant = NULL;
+
+        printf("\nInforme o nome do contato para excluir: ");
+        getchar();
+        scanf("%[^\n]", &nome);
+        getchar();
+
+    while(p != NULL && ret != 0){
+        ret = strncmp(p->nome, nome, 7);
+        ant = p;
+        p = p->prox;
+    }
+
+            if(p != NULL){
+                if(ant == NULL) {
+                    l->prim = p->prox;
+                }
+                else {
+                    ant->prox = p->prox;
+                }
+                free(p);
+            }
+    }
+
+    else
+    printf("\nOpcao invalida!\n");
 }
 
 int menu () {
@@ -62,10 +181,11 @@ int menu () {
     return opt;
 }
 
-void opcao(Lista *l, int op) {
-	switch(op){
-		case 0:
 
+void opcao(Lista *l, int opt) {
+	switch(opt){
+		case 0:
+            liberaAgenda(l);
 			break;
 
 		case 1:
@@ -77,11 +197,11 @@ void opcao(Lista *l, int op) {
 			break;
 
 		case 3:
-
+            busImp(l);
 			break;
 
 		case 4:
-
+            editRemov(l);
 			break;
 
 		default:
@@ -92,6 +212,7 @@ void opcao(Lista *l, int op) {
 int main () {
     Lista* l = criaLista();
     int opt;
+
 
     do{
     opt = menu ();
